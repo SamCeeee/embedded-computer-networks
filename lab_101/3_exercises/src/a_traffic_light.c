b4f8b87efa34d4108f4b8a61def457072c735140
SamCeeee <43850230+SamCeeee@users.noreply.github.com>
2018-10-04 14:55:26 +0100

    Task 3 WIP
    Tragic lights do not works

--- lab_101/3_exercises/src/a_traffic_light.c
+++ lab_101/3_exercises/src/a_traffic_light.c
@@ -14,10 +14,35 @@
 #include "gpio.h"
 
 // map the led to GPIO PA8
-gpio_pin_t led = {PI_1, GPIOI, GPIO_PIN_1};
+gpio_pin_t redled = {PI_1, GPIOI, GPIO_PIN_1};
+gpio_pin_t amberled = {PB_14, GPIOB, GPIO_PIN_14};
+gpio_pin_t greenled = {PB_15, GPIOB, GPIO_PIN_15};
 
 // this is the main method
 int main()
 {
-
+	// Initialise the HAl library
+	HAL_Init();
+	init_sysclk_216MHz();
+	
+	init_gpio(redled, OUTPUT);
+	init_gpio(amberled, OUTPUT);
+	init_gpio(greenled, OUTPUT);
+	
+	
+	write_gpio(redled, LOW);
+	write_gpio(amberled, LOW);
+	write_gpio(greenled, LOW);
+	
+	while(1)
+	{
+		write_gpio(redled, HIGH);
+		HAL_Delay(1000);
+		write_gpio(redled, LOW);
+		HAL_Delay(1000);
+		toggle_gpio(amberled);
+		toggle_gpio(greenled);
+		HAL_Delay(1000);
+		toggle_gpio(greenled);
+	}
 }
